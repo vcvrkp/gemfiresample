275fa6691aa7b31d54f58324b7e42661ebca7b8f
vcvr <ramakrishna.vcv@emc.com>
2019-10-21 11:16:13 +0530

    first commit

---
+++ src/test/java/com/example/gemfiresample/GemfiresampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gemfiresample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GemfiresampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
