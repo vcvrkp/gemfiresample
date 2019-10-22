895485cabb95dcaab56e8ee9cb16a435044b85ea
vcvr <ramakrishna.vcv@emc.com>
2019-10-22 17:32:31 +0530

    Example of pairing with a external cache having an inbuilt server also. inbuilt is a server name. 40421 is the port we are running. and connecting to the locator where other server connected to.

--- src/main/java/com/example/gemfiresample/GemfiresampleApplication.java
+++ src/main/java/com/example/gemfiresample/GemfiresampleApplication.java
@@ -6,7 +6,7 @@ import org.springframework.data.gemfire.config.annotation.CacheServerApplication
 import org.springframework.data.gemfire.config.annotation.EnableEntityDefinedRegions;
 
 @SpringBootApplication
-@CacheServerApplication(port=40421,autoStartup=true,maxConnections=3,locators="localhost[10334]")
+@CacheServerApplication(name="inbuilt",port=40421,autoStartup=true,maxConnections=3,locators="localhost[10334]")
 @EnableEntityDefinedRegions(basePackages = "com.example.gemfiresample.model")
 public class GemfiresampleApplication {
 
